82e4e8055a3236e2586902248d3927fd3a2f3f2d
jw54kysa <jonathan.weske@gmail.com>
2024-11-18 12:14:36 +0100

    change nodes to 3

--- CIFAR10/runExp.sh
+++ CIFAR10/runExp.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --job-name=feddc
 #SBATCH --partition=paula
-#SBATCH -N 2
-#SBATCH --ntasks=2
-#SBATCH --gpus=a30:4
-#SBATCH --mem=128G                    # Adjust memory as needed
-#SBATCH --time=24:00:00             # Set a realistic time limit
+#SBATCH -N 3
+#SBATCH --ntasks=3
+#SBATCH --gpus=a30:6
+#SBATCH --mem=64G
+#SBATCH --time=24:00:00
 #SBATCH -o log/%x.out-%j
 #SBATCH -e log/%x.error-%j
 #SBATCH --mail-type=BEGIN,END
@@ -20,10 +20,7 @@ numrounds=10000
 schedrounds=2500
 
 # sbatch seed
-seed=1 #$SLURM_ARRAY_TASK_ID
-
-#for seed in 1 2 #3
-#do
+seed=1
 
 info=""
 daisy=1
@@ -37,15 +34,3 @@ srun singularity exec --nv FEDDC.sif python3.9 -u feddc_CIFAR10_pytorch.py --opt
 info="vanillafeddc"
 # vanilla daisy chaining
 srun singularity exec --nv FEDDC.sif python3.9 -u feddc_CIFAR10_pytorch_vanilla_daisy_chaining.py --optimizer SGD --train-batch-size $batch_size --lr 0.1 --lr-schedule-ep ${schedrounds} --lr-change-rate 0.5 --num-clients $numclients --num-rounds $numrounds --num-samples-per-client $numdat --report-rounds 250 --daisy-rounds $daisy --aggregate-rounds $avg --seed $seed | tee CompExp_Cifar10_${info}_${numclients}cl_n${numdat}_b${batch_size}_d${daisy}_a${avg}_r${numrounds}_s${seed}.log
-
-
-#daisy=10001
-#avg=1
-#srun singularity exec --nv FEDDC.sif python3.9 -u feddc_CIFAR10_pytorch.py --optimizer SGD --train-batch-size $batch_size --lr 0.1 --lr-schedule-ep ${schedrounds} --lr-change-rate 0.5 --num-clients $numclients --num-rounds $numrounds --num-samples-per-client $numdat --report-rounds 250 --daisy-rounds $daisy --aggregate-rounds $avg --seed $seed | tee CompExp_Cifar10_${numclients}cl_n${numdat}_b${batch_size}_d${daisy}_a${avg}_lr0_1_schedule${schedrounds}_r${numrounds}_s${seed}.log
-#
-#
-#daisy=10001
-#avg=10
-#
-#srun singularity exec --nv FEDDC.sif python3.9 -u feddc_CIFAR10_pytorch.py  --optimizer SGD --train-batch-size $batch_size --lr 0.1 --lr-schedule-ep ${schedrounds} --lr-change-rate 0.5 --num-clients $numclients --num-rounds $numrounds --num-samples-per-client $numdat --report-rounds 250 --daisy-rounds $daisy --aggregate-rounds $avg --seed $seed | tee CompExp_Cifar10_${numclients}cl_n${numdat}_b${batch_size}_d${daisy}_a${avg}_lr0_1_schedule${schedrounds}_r${numrounds}_s${seed}.log
-#done
