e71d7732794f9aa1d11a3ce017b2d12938b9290e
jw54kysa <jonathan.weske@gmail.com>
2024-11-27 20:49:35 +0100

    run exps alone

--- CIFAR10/runMyExp.sh
+++ CIFAR10/runMyExp.sh
@@ -2,8 +2,8 @@
 #SBATCH --job-name=runMyExp
 #SBATCH --partition=paula
 #SBATCH -N 1
-#SBATCH --ntasks=2
-#SBATCH --gpus=a30:2
+#SBATCH --ntasks=1
+#SBATCH --gpus=a30:1
 #SBATCH --mem=16G
 #SBATCH --time=1-00:00:00
 #SBATCH -o log/%x.out-%j
@@ -27,29 +27,7 @@ avg=10
 iid='randsize'
 min=8
 max=64
-per='rand'
-
-srun singularity exec --nv FEDDC.sif \
-python3.9 -u feddc_CIFAR10_pytorch.py \
-    --optimizer SGD \
-    --train-batch-size $batch_size \
-    --lr 0.1 \
-    --lr-schedule-ep ${schedrounds} \
-    --lr-change-rate 0.5 \
-    --num-clients $numclients \
-    --num-rounds $numrounds \
-    --num-samples-per-client $numdat \
-    --report-rounds 250 \
-    --daisy-rounds $daisy \
-    --iid-data $iid \
-    --min-samples $min \
-    --max-samples $max \
-    --permutation $per \
-    --aggregate-rounds $avg \
-    --seed $seed \
-    | tee CompExp_Cifar10_iid${iid}_${per}${min}${max}_nc${numclients}cl_n${numdat}_b${batch_size}_d${daisy}_a${avg}_lr0_1_schedule${schedrounds}_r${numrounds}_s${seed}.log
-
-per='prob'
+per='rand' #'prob'
 
 srun singularity exec --nv FEDDC.sif \
 python3.9 -u feddc_CIFAR10_pytorch.py \
