5084681ceb7d74d2ec385de818c7e7d51419a58e
jw54kysa <jonathan.weske@gmail.com>
2025-01-06 10:35:28 +0100

    go to 5000 rounds

--- CIFAR10/runMyExp.sh
+++ CIFAR10/runMyExp.sh
@@ -17,7 +17,7 @@ export PYTHONNOUSERSITE=1
 numclients=150
 batch_size=64
 numdat=64
-numrounds=1000
+numrounds=5000
 #schedrounds=2500
 
 seed=1
@@ -35,7 +35,7 @@ srun singularity exec --nv FEDDC.sif \
 python3.9 -u feddc_CIFAR10_pytorch_test_prob_perm.py \
     --optimizer SGD \
     --train-batch-size $batch_size \
-    --lr 0.1 \
+    --lr 0.01 \
     --num-clients $numclients \
     --num-rounds $numrounds \
     --num-samples-per-client $numdat \
